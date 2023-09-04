266b24fbeb897cb03ee15c087a32e5b3f980a230
Adams-Ghub <bamfoadamsf@gmail.com>
2023-09-04 13:08:41 +0000

    Update login and register pages

---
+++ src/screens/admin/registrations.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Text,TouchableOpacity,} from 'react-native'
+
+const Registrations=()=> {
+  return (
+    <Text>Registrations page</Text>
+  )
+}
+
+export default Registrations
--- src/screens/login.js
+++ src/screens/login.js
@@ -9,34 +9,42 @@ import {
 } from 'react-native';
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { UserLogin } from '../../../redux/users/usersActions';
+import { UserLogin } from '../redux/users/usersAction';
+import { MaterialIcons } from '@expo/vector-icons';
 import Modal from 'react-native-modal';
 
 export default function Login({ navigation }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
   const [isModalVisible, setModalVisible] = useState(false);
   const dispatch = useDispatch();
-  const { user } = useSelector((state) => state.users);
+  const { user,logged } = useSelector((state) => state.users);
 
   const handleLogin = () => {
-    dispatch(UserLogin({email, password}));
-    console.log('email/password:', email + '/' + password);
+    dispatch(UserLogin({ email, password }));
+    
   };
 
   const handleModalClose = () => {
     setModalVisible(false);
-    navigation.navigate('Login');
+    
   };
 
   useEffect(() => {
-    if (user) {
+    if (logged) {
+      setEmail('')
+      setPassword('')
       console.log('role:', user.details.role);
-      user.details.role === 'customer'
-        ? navigation.navigate('ClientWelcome')
-        : navigation.navigate('PharmacistWelcome');
+      if (user.details.role === 'tutor') {
+        navigation.navigate('TutorWelcome');
+      } else if (user.details.role === 'parent') {
+        navigation.navigate('ParentWelcome');
+      } else {
+        navigation.navigate('AdminWelcome');
+      }
     }
-  }, [user]);
+  }, [logged,dispatch]);
 
   return (
     <View style={styles.container}>
@@ -54,12 +62,30 @@ export default function Login({ navigation }) {
         </View>
         <View style={styles.passwordLabelInputContainer}>
           <Text style={styles.passwordText}>Password</Text>
-          <TextInput
-            secureTextEntry
-            style={styles.passwordInput}
-            value={password}
-            onChangeText={(text) => setPassword(text)}
-          />
+          <View style={styles.visibilityPasswordInputContainer}>
+            <TextInput
+              secureTextEntry={!showPassword} // Toggle secureTextEntry based on showPassword state
+              style={styles.passwordInput}
+              onChangeText={(text) => setPassword(text)}
+            />
+            {/* Password visibility toggle */}
+            <TouchableOpacity
+              onPress={() => setShowPassword(!showPassword)}
+              style={styles.passwordVisibilityToggle}
+            >
+              <Text>
+                {showPassword ? (
+                  <MaterialIcons
+                    name="visibility-off"
+                    size={30}
+                    color="#39449c"
+                  />
+                ) : (
+                  <MaterialIcons name="visibility" size={30} color="#39449c" />
+                )}
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
         <View style={styles.loginButtonContainer}>
           <TouchableOpacity
@@ -87,7 +113,10 @@ export default function Login({ navigation }) {
       <Modal isVisible={isModalVisible}>
         <View style={styles.modalContainer}>
           <Text style={styles.modalText}>User signed up successfully!</Text>
-          <TouchableOpacity style={styles.modalButton} onPress={handleModalClose}>
+          <TouchableOpacity
+            style={styles.modalButton}
+            onPress={handleModalClose}
+          >
             <Text style={styles.modalButtonText}>OK</Text>
           </TouchableOpacity>
         </View>
@@ -100,35 +129,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: '#03C043',
+    backgroundColor: '#fff',
     justifyContent: 'flex-start',
     width: '100%',
     margin: 0,
     padding: 0,
   },
   headingSection: {
-    flex: 0.4,
+    flex: 0.5,
     height: '10%',
+    backgroundColor: '#3944bc',
     justifyContent: 'center',
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
   },
   headingText: {
     alignSelf: 'center',
     fontSize: 35,
-    color: '#fefefe',
+    color: '#fff',
     fontWeight: '300',
   },
   bottomSection: {
-    flex: 0.6,
-    backgroundColor: '#fff',
-    borderTopLeftRadius: 50,
-    borderTopRightRadius: 50,
+    flex: 0.5,
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
     paddingTop: 90,
     paddingHorizontal: 20,
   },
   emailLabelInputContainer: {
     borderStyle: 'solid',
     borderBottomWidth: 1,
-    borderBottomColor: '#03C043',
+    borderBottomColor: '#3944bc',
     marginBottom: 30,
   },
   emailText: {
@@ -146,7 +177,7 @@ const styles = StyleSheet.create({
   passwordLabelInputContainer: {
     borderStyle: 'solid',
     borderBottomWidth: 1,
-    borderBottomColor: '#03C043',
+    borderBottomColor: '#3944bc',
     marginBottom: 30,
   },
   passwordText: {
@@ -160,9 +191,13 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     color: '#050505',
     fontSize: 18,
+    width: '90%',
+  },
+  visibilityPasswordInputContainer: {
+    flexDirection: 'row',
   },
   loginButton: {
-    backgroundColor: '#03C043',
+    backgroundColor: '#3944bc',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
@@ -184,7 +219,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   forgotPasswordText: {
-    color: '#03C043',
+    color: '#3944bc',
     fontSize: 18,
     fontWeight: '400',
   },
@@ -198,7 +233,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
   createAccountButtonText: {
-    color: '#03C043',
+    color: '#3944bc',
     fontSize: 18,
     marginVertical: 2,
   },
@@ -214,7 +249,7 @@ const styles = StyleSheet.create({
     marginBottom: 16,
   },
   modalButton: {
-    backgroundColor: '#03C043',
+    backgroundColor: '#3944bc',
     borderRadius: 5,
     paddingVertical: 8,
     paddingHorizontal: 16,
