58876e117e3574b8df706f90939cf25ceccd60fc
Adams-Ghub <bamfoadamsf@gmail.com>
2023-09-04 13:04:59 +0000

    Update all navigations

--- src/navigation/admin-navigation.js
+++ src/navigation/admin-navigation.js
@@ -1,48 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { DrawerActions } from '@react-navigation/native';
 import DrawerItem from '../components/drawer-item';
-import Account from '../screens/pharmacist/profile-screen';
-import PrescriptionScreen from '../screens/pharmacist/prescription-screen';
-import AddPrescriptionScreen from '../screens/pharmacist/add-prescription-screen';
-import Feedback from '../screens/pharmacist/feedback-screen';
-import Home from '../screens/pharmacist/pharmacist-welcome-screen';
+import Profile from '../screens/admin/admin-profile';
+import Registrations from '../screens/admin/registrations';
 import { TouchableOpacity, View, Text, Image } from 'react-native';
-import {
-  Entypo,
-  SimpleLineIcons,
-  Fontisto,
-  Ionicons,
-} from '@expo/vector-icons';
+import { useDispatch, useSelector } from 'react-redux';
+import { Logout } from '../redux/users/usersAction.js';
+import { Entypo, AntDesign, MaterialIcons, Ionicons } from '@expo/vector-icons';
 import profileImg from '../../assets/profile.png';
 
 const Stack = createNativeStackNavigator();
-const ParentNavigatior=()=> {
+const ParentNavigatior = () => {
   return (
     <Stack.Navigator
       screenOptions={{
         header: () => null,
       }}
       initialRouteName="PrescriptionScreen"
-      
     >
       <Stack.Screen
         name="PrescriptionScreen"
         // options={{ header: () => {} }}
         component={PrescriptionScreen}
-      />    
+      />
       <Stack.Screen
         name="AddPrescription"
         // options={{ header: () => {} }}
         component={AddPrescriptionScreen}
-      />    
+      />
     </Stack.Navigator>
   );
-}
+};
 
 function AdminNavigation({ navigation }) {
   const Drawer = createDrawerNavigator();
+  const dispatch = useDispatch();
+  const { user, logged } = useSelector((state) => state.users);
+  const handleLogout = () => {
+    dispatch(Logout());
+  };
+
+  useEffect(
+    () => {
+      if (logged === false) {
+        navigation.navigate('Login');
+      }
+    },
+    [logged],
+    dispatch
+  );
+
+  const displayName = logged ? user.details.fullName.split(' ') : '';
+
   return (
     <Drawer.Navigator
       screenOptions={{
@@ -76,55 +87,55 @@ function AdminNavigation({ navigation }) {
                   width: 25,
                   height: 25,
                   borderWidth: 1.5,
-                  borderColor: '#03C043',
+                  borderColor: '#3944bc',
                   borderRadius: 50,
                 }}
               />
 
               <Text
                 style={{
-                  margin: 0,
                   padding: 0,
                   fontWeight: '600',
                   fontSize: 16,
+                  marginRight: 15,
                 }}
               >
-                Ivy Osardu
+                {displayName[0]}
               </Text>
+              <TouchableOpacity onPress={handleLogout}>
+                <AntDesign name="logout" size={16} color="blue" />
+              </TouchableOpacity>
             </View>
           );
         },
         headerStyle: {
           backgroundColor: '#ddd',
           height: 70,
+          borderBottomWidth: 2,
+          borderBottomColor: '#3944bc'
         },
         headerTitleContainerStyle: {
           top: 0, // Adjust the top position to align with the drawer content
         },
         headerStatusBarHeight: 15,
         drawerStyle: {
-          backgroundColor: '#03C043',
+          backgroundColor: '#3944bc',
           width: '55%',
           paddingTop: '10%',
         },
       }}
     >
       <Drawer.Screen
-        name="Home"
-        component={Home}
+        name="Profile"
+        component={Profile}
         options={{
           drawerLabel: () => {
             return (
               <DrawerItem
                 icon={
-                  <SimpleLineIcons
-                    name="home"
-                    style={{}}
-                    size={16}
-                    color="#000"
-                  />
+                  <AntDesign name="user" style={{}} size={16} color="#fff" />
                 }
-                title="Home"
+                title="Profie"
               />
             );
           },
@@ -133,22 +144,23 @@ function AdminNavigation({ navigation }) {
           },
         }}
       />
+
       <Drawer.Screen
-        name="Account"
-        component={Account}
+        name="Registrations"
+        component={Registrations}
         options={{
           drawerLabel: () => {
             return (
               <DrawerItem
                 icon={
-                  <SimpleLineIcons
-                    name="user"
+                  <MaterialIcons
+                    name="app-registration"
                     style={{}}
                     size={16}
-                    color="#000"
+                    color="#fff"
                   />
                 }
-                title="Account"
+                title="Registrations"
               />
             );
           },
@@ -157,55 +169,8 @@ function AdminNavigation({ navigation }) {
           },
         }}
       />
-      <Drawer.Screen
-        name="Prescription"
-        component={PrescriptionNavigatior}
-        options={{
-          drawerLabel: () => {
-            return (
-              <DrawerItem
-                icon={
-                  <Fontisto
-                    name="prescription"
-                    style={{}}
-                    size={16}
-                    color="#000"
-                  />
-                }
-                title="Prescription"
-              />
-            );
-          },
-          drawerItemStyle: {
-            backgroundColor: 'transparent',
-          },
-        }}
-      />
-      <Drawer.Screen
-        name="Feedback"
-        component={Feedback}
-        options={{
-          drawerLabel: () => {
-            return (
-              <DrawerItem
-                icon={
-                  <Ionicons
-                    name="ios-chatbubbles-outline"
-                    style={{}}
-                    size={16}
-                    color="#000"
-                  />
-                }
-                title="Feedback"
-              />
-            );
-          },
-          drawerItemStyle: {
-            backgroundColor: 'transparent',
-          },
-        }}
-      />
-        {/* <Drawer.Screen name="AddPrescription" component={AddPrescriptionScreen} /> */}
+    
+      {/* <Drawer.Screen name="AddPrescription" component={AddPrescriptionScreen} /> */}
     </Drawer.Navigator>
   );
 }
