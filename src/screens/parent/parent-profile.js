a86446ebc16b60540676ce28d99fc99b2ed1cc6d
Adams-Ghub <bamfoadamsf@gmail.com>
2023-09-11 10:22:48 +0000

    Build parent profile page

--- src/screens/parent/parent-profile.js
+++ src/screens/parent/parent-profile.js
@@ -1,10 +1,223 @@
-import React from 'react'
-import {Text,TouchableOpacity,} from 'react-native'
+import React, { useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+import {
+  View,
+  TextInput,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+} from 'react-native';
 
-const ParentProfile=()=> {
+function ParentProfile() {
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [5, 5],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
   return (
-    <Text>Profile page</Text>
-  )
+    <View style={styles.mainContainer}>
+      <View style={styles.imageMajorContainer}>
+        <TouchableOpacity
+          onPress={pickImage}
+          style={styles.profileImageContainer}
+        >
+          {image ? (
+            image && (
+              <Image
+                source={{ uri: image }}
+                style={{ width: '100%', height: '100%', alignSelf: 'center' }}
+              />
+            )
+          ) : (
+            <Text style={styles.selectImgText}>select an image</Text>
+          )}
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bottomSection}>
+        <View style={styles.usernameAndInputContainer}>
+          <View style={styles.usernameEditContainer}>
+            <Text style={[styles.usernameText, styles.labelText]}>
+              Full name
+            </Text>
+            <TouchableOpacity style={styles.editButton}>
+              <Text style={styles.editButtonText}>edit</Text>
+            </TouchableOpacity>
+          </View>
+          <TextInput style={styles.allTextInput} />
+        </View>
+        <View style={styles.emailAndInputContainer}>
+          <View style={styles.emailEditContainer}>
+            <Text style={[styles.emailText, styles.labelText]}>email</Text>
+            <TouchableOpacity style={styles.editButton}>
+              <Text style={styles.editButtonText}>edit</Text>
+            </TouchableOpacity>
+          </View>
+          <TextInput style={styles.allTextInput} />
+        </View>
+
+        <View style={styles.phoneAndInputContainer}>
+          <View style={styles.phoneEditContainer}>
+            <Text style={[styles.phoneText, styles.labelText]}>phone</Text>
+            <TouchableOpacity style={styles.editButton}>
+              <Text style={styles.editButtonText}>edit</Text>
+            </TouchableOpacity>
+          </View>
+          <TextInput style={styles.allTextInput} />
+        </View>
+        <View style={styles.pharmacyAndInputContainer}>
+          <View style={styles.pharmacyEditContainer}>
+            <Text style={[styles.pharmacyText, styles.labelText]}>
+              location
+            </Text>
+            <TouchableOpacity style={styles.editButton}>
+              <Text style={styles.editButtonText}>edit</Text>
+            </TouchableOpacity>
+          </View>
+          <TextInput style={styles.allTextInput} />
+        </View>
+        <View style={styles.regNumberAndInputContainer}>
+          <View style={styles.regNumberEditContainer}>
+            <View style={styles.coordinateContainer}>
+              <View style={styles.coordinateLabelInputContainer}>
+                <Text style={[styles.regNumberText, styles.labelText]}>
+                  coordinates
+                </Text>
+                <TextInput style={styles.allTextInput} />
+              </View>
+              <TouchableOpacity>
+                <Text>Get coordinates</Text>
+              </TouchableOpacity>
+            </View>
+            <TouchableOpacity style={styles.editButton}>
+              <Text style={styles.editButtonText}>edit</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View>
+          <TouchableOpacity style={styles.updateButton}>
+            <Text style={styles.updateButtonText}>update</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
 }
 
-export default ParentProfile
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginHorizontal: 15,
+  },
+  imageMajorContainer: {
+    flex: 0.45,
+    width: '100%',
+    marginTop: 10,
+  },
+  selectImgText: {
+    fontSize: 20,
+    position: 'relative',
+    top: '40%',
+    alignSelf: 'center',
+  },
+  profileImageContainer: {
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    width: '100%',
+    height: '100%',
+    marginVertical: 0,
+    paddingVertical: 0,
+  },
+  bottomSection: {
+    flex: 0.5,
+    width: '100%',
+  },
+  usernameEditContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  emailEditContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  pharmacyEditContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  regNumberEditContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  phoneEditContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  allTextInput: {
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#333',
+    borderRadius: 5,
+    paddingHorizontal: 5,
+    fontSize: 14,
+    height: 24,
+  },
+  editButtonText: {
+    color: '#3944bc',
+    fontSize: 16,
+  },
+  labelText: {
+    fontSize: 18,
+    fontWeight: '700',
+  },
+  usernameAndInputContainer: {
+    marginBottom: 10,
+  },
+  emailAndInputContainer: {
+    marginBottom: 10,
+  },
+  pharmacyAndInputContainer: {
+    marginBottom: 10,
+  },
+  phoneAndInputContainer: {
+    marginBottom: 10,
+  },
+  regNumberAndInputContainer: {
+    marginBottom: 10,
+  },
+  updateButton: {
+    backgroundColor: '#3944bc',
+    paddingVertical: 8,
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  coordinateContainer:{
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  coordinateLabelInputContainer:{
+    width:'65%',
+    gap: 5,
+  },
+  updateButtonText: {
+    color: '#fff',
+    fontSize: 18,
+  },
+});
+
+export default ParentProfile;
