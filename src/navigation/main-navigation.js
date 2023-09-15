d7ecd799994d905b656b88bc6fcb723bdaa861fc
Adams-Ghub <bamfoadamsf@gmail.com>
2023-09-15 11:16:01 +0000

    improve main navigation to present the right screen based on user role

--- src/navigation/main-navigation.js
+++ src/navigation/main-navigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useSelector } from 'react-redux';
 import Login from '../screens/login';
 import Signup from '../screens/signup';
 import ParentNavigation from './parent-navigation';
@@ -7,34 +8,46 @@ import AdminNavigation from './admin-navigation';
 import TutorNavigation from './tutor-navigation';
 
 function MainNavigation() {
+  const { user, logged } = useSelector((state) => state.users);
+
   const Stack = createNativeStackNavigator();
+
   return (
     <Stack.Navigator>
-      <Stack.Screen
-        name="Login"
-        component={Login}
-        options={{ header: () => {} }}
-      />
-      <Stack.Screen
-        name="Signup"
-        component={Signup}
-        options={{ header: () => {} }}
-      />
-      <Stack.Screen
-        name="ParentWelcome"
-        component={ParentNavigation}
-        options={{ header: () => {} }}
-      />
-      <Stack.Screen
-        name="TutorWelcome"
-        component={TutorNavigation}
-        options={{ header: () => {} }}
-      />
-      <Stack.Screen
-        name="AdminWelcome"
-        component={AdminNavigation}
-        options={{ header: () => {} }}
-      />
+      {logged ? (
+        user.details.role === 'admin' ? (
+          <Stack.Screen
+            name="AdminWelcome"
+            component={AdminNavigation}
+            options={{ header: () => {} }}
+          />
+        ) : user.details.role === 'parent' ? (
+          <Stack.Screen
+            name="ParentWelcome"
+            component={ParentNavigation}
+            options={{ header: () => {} }}
+          />
+        ) : (
+          <Stack.Screen
+            name="TutorWelcome"
+            component={TutorNavigation}
+            options={{ header: () => {} }}
+          />
+        )
+      ) : (
+        <>
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ header: () => {} }}
+          />
+          <Stack.Screen
+            name="Signup"
+            component={Signup}
+            options={{ header: () => {} }}
+          />
+        </>
+      )}
     </Stack.Navigator>
   );
 }
