fed30fe59091cb817a28f8928fc8c6e242b04288
Adams-Ghub <bamfoadamsf@gmail.com>
2023-09-18 09:15:09 +0000

    Create rootreduce to add persistence

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users/usersSlice';
+import userRequests from './requests/requestsSlice'
 
 const rootReducer = combineReducers({
     users: usersReducer,
+    requests:userRequests
 });
 
 export default rootReducer;
